caa066fb0e4fec6194afc584d91f8e633ad9adcc
Ahmadhumayoun <81822752+Ahmadhumayoun@users.noreply.github.com>
2026-02-27 19:58:12 +0500

    Moving Of Files
    --

---
+++ FirstCode.py
@@ -0,0 +1,21 @@
+while True:
+    choice = int(input("Enter 1 for account Opening \n"
+                   " Enter 2 for Balance Check \n Enter 3 for WithDrawl"
+                   " \n Enter 4 for Deposit:"
+                   "\n Enter 5 for Exit:"))
+    if choice ==1:
+        print("1")
+        # Create Account
+    elif choice ==2:
+        print()
+        # Balance Check
+    elif choice ==3:
+        print()
+        # WithDrawl
+    elif choice ==4:
+        print()
+    elif choice ==5:
+        break
+
+#Create dictionary at runtime
+# Username will be key and age , balance will be values
---
+++ Tasks.py
@@ -0,0 +1,13 @@
+"""
+Task: Create a script that asks for a user's name and birth year.
+
+Requirements:
+
+Calculate their age (assume the current year is 2026).
+
+Use a string method to ensure their name is properly capitalized (e.g., "bob" becomes "Bob").
+
+Print a message like: "Hello [Name], you are [Age] years old!"
+
+Use an if statement to print an extra message if they are over 18.
+"""
