845c0984815aba46b8ba01e7bae07cf0b1287bbc
Ahmadhumayoun <81822752+Ahmadhumayoun@users.noreply.github.com>
2026-03-04 20:37:26 +0500

    Numpy Slicing Lecture
    Numpy Slicing Coverage

--- numpyTutorial.py
+++ numpyTutorial.py
@@ -1,8 +1,3 @@
 import numpy as np
-farr = np.array(range(240))
-farr = farr.reshape(2,2,3,4,5)
-print(farr)
-#177
-#71
-#240
-#0
+slicingMatric = np.array(range(60)).reshape(2,5,6)
+print(slicingMatric[0,1:4,1:6:2])
