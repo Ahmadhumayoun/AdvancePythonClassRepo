d68731ad4521863f35a5c2431ec1c24d7e66c4cb
toptalahmad-prog <toptal.ahmad@gmail.com>
2026-03-16 20:43:59 +0500

    Quiz System more advances 8:43 3/16
    .

--- QuizSystem.py
+++ QuizSystem.py
@@ -25,18 +25,4 @@ for i in range(10):
 userdataDictionary = {"Names":["Ahmad","Ali","Pyth"],"Score":[9,10,5]}
 
 userDataFrame = pd.DataFrame(userdataDictionary)
-while True:
-    print("Enter 1 if you are new and want to take the test Enter 2 For taking the test again \nenter 3 for exit")
-    choice = input("Enter your choice:")
-    if choice == "1":
-        name = input("Enter your name:")
-        # Quiz Proceed
 
-    elif choice == "2":
-        name = input("Enter your name:")
-        if name in list(userDataFrame["Names"]):
-            # Quiz proceed
-    elif choice == "3":
-        break
-    else :
-        pass
