c94ec36ae73dfb68f514fdd3f1c5aee10999b7aa
Ahmadhumayoun <81822752+Ahmadhumayoun@users.noreply.github.com>
2026-04-03 20:43:45 +0500

    MultiMedia Scrape
    .

--- DataScrapingClassLearning.py
+++ DataScrapingClassLearning.py
@@ -7,17 +7,17 @@ import pandas as pd
 # with open("PageSource.txt","w") as f:
 #     f.write(pageSource)
 
-with open("PageSource.txt","r",encoding="utf-8") as f:
-    sourceCode = f.read()
-    s = bs(sourceCode,"html.parser")
-    # s = s.prettify()
-    countryData ={"Country Name": [], "Country Capital": [], "Country Population": [], "Country Area": []}
-    countryNames = s.find_all("h3",{"class":"country-name"})
-    countrycapitals = s.find_all("span", {"class": "country-capital"})
-    countrypopulation = s.find_all("span", {"class": "country-population"})
-    countryArea = s.find_all("span", {"class": "country-area"})
-    for i in range(len(countryNames)):
-        countryData["Country Name"].append(countryNames[i].text.strip())
-        countryData["Country Capital"].append(countrycapitals[i].text.strip())
-        countryData["Country Population"].append(countrypopulation[i].text.strip())
-        countryData["Country Area"].append(countryArea[i].text.strip())
+# with open("PageSource.txt","r",encoding="utf-8") as f:
+#     sourceCode = f.read()
+#     s = bs(sourceCode,"html.parser")
+#     # s = s.prettify()
+#     countryData ={"Country Name": [], "Country Capital": [], "Country Population": [], "Country Area": []}
+#     countryNames = s.find_all("h3",{"class":"country-name"})
+#     countrycapitals = s.find_all("span", {"class": "country-capital"})
+#     countrypopulation = s.find_all("span", {"class": "country-population"})
+#     countryArea = s.find_all("span", {"class": "country-area"})
+#     for i in range(len(countryNames)):
+#         countryData["Country Name"].append(countryNames[i].text.strip())
+#         countryData["Country Capital"].append(countrycapitals[i].text.strip())
+#         countryData["Country Population"].append(countrypopulation[i].text.strip())
+#         countryData["Country Area"].append(countryArea[i].text.strip())
